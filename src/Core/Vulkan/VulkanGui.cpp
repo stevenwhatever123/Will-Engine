d67e721cbe9c21b83ca7f6941d7f1b908210c3c4
stevenwhatever123 <60275831+stevenwhatever123@users.noreply.github.com>
2022-10-05 19:16:50 +0100

    ImGui tree node displaying fix

--- src/Core/Vulkan/VulkanGui.cpp
+++ src/Core/Vulkan/VulkanGui.cpp
@@ -128,8 +128,11 @@ void VulkanGui::update(std::vector<Mesh*>& meshes, std::vector<Material*>& mater
 
 	for (u32 i = 0; i < meshes.size(); i++)
 	{
+		ImGui::PushID(i);
+
 		if (ImGui::TreeNode(meshes[i]->name.c_str()))
 		{
+
 			ImGui::DragFloat3("Position", &meshes[i]->transformPosition.x, 0.1f);
 
 			ImGui::Text("Material: %s", materials[meshes[i]->materialIndex]->name.c_str());
@@ -138,6 +141,8 @@ void VulkanGui::update(std::vector<Mesh*>& meshes, std::vector<Material*>& mater
 
 			ImGui::TreePop();
 		}
+
+		ImGui::PopID();
 	}
 	ImGui::End();
 
